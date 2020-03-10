d948349085ae9b4b756c39bdbcd83e8e6cc01b4f
Jimmy <born1031.jimmy@gmail.com>
2020-03-10 22:59:12 +0800

    fix
    fix bug

--- index.js
+++ index.js
@@ -20,5 +20,5 @@ app.post('/', linebotParser);
 // Convert port for heroku
 var server = app.listen(process.env.PORT || 8080, function(){
     var port = server.address().port;
-    console.log('App now running on port: ' + prot);
+    console.log('App now running on port: ' + port);
 });
