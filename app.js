805d16819b73caacb6a4292b7f06a1ee59ef2a9b
Jimmy <born1031.jimmy@gmail.com>
2020-03-10 20:40:59 +0800

    v0.0.1

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const linebot = require('linebot');
 var bot = linebot({
     channelId: '1653936288',
     channelSecret: '9914a93854b110443120b46240eae039',
-    channelAccessToken: '9914a93854b110443120b46240eae039'
+    channelAccessToken: 'zURpT1GazPArNvwi4V0r41dFRSe9vhU48aQLqPxiBKOVxtJW/be9aViXod+XCv9Cr94n5F3EbXkDCPhWD4325w4fBkRyvYL5I0Uq7bJiOz/9G8pu90qAGo160ftuCwATlx78PNSHXYHibI42GrD8CwdB04t89/1O/w1cDnyilFU='
 });
 
 // When receive messages
