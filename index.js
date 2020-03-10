972e86ee62b86980016fdf33591879b5b01715b7
Jimmy <born1031.jimmy@gmail.com>
2020-03-10 22:54:55 +0800

    V0.0.0

---
+++ index.js
@@ -0,0 +1,24 @@
+var linebot = require('linebot');
+var express = require('express');
+
+var bot = linebot({
+    channelId: '1653936288',
+    channelSecret: '9914a93854b110443120b46240eae039',
+    channelAccessToken: '72jIJyLaqwIKgS5XDmxFx++2s2lcMQXp9ARTPBC7BeHRlObrN7swCwwfJPDcso6nr94n5F3EbXkDCPhWD4325w4fBkRyvYL5I0Uq7bJiOz+Yeiof37Hi4WYZpzoIThvUW+zCppKpLF5pGmIoQ6phjwdB04t89/1O/w1cDnyilFU='
+});
+
+bot.on('message', function(event){
+    // Print event.
+    console.log(event);
+});
+
+const app = express();
+const linebotParser = bot.parser();
+
+app.post('/', linebotParser);
+
+// Convert port for heroku
+var server = app.listen(process.env.PORT || 8080, function(){
+    var port = server.address().port;
+    console.log('App now running on port: ' + prot);
+});
