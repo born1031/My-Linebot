c552a817b32ee822a85d06b718af53188e3da21e
Jimmy <born1031.jimmy@gmail.com>
2020-03-12 20:33:06 +0800

    v0.0.3 test

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function _bot() {
         console.log(event);
 
         // Get user profile.
-        var userProfile = bot.getUserProfile(event.source.useId);
+        var userProfile = event.source.profile(event.source.useId);
         console.log('This is profile');
         console.log(userProfile);
         
