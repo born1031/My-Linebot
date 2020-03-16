f2f5e145450a4e58174cabe7768387ea882c02a2
Jimmy <born1031.jimmy@gmail.com>
2020-03-16 15:19:38 +0800

    v0.1.0 test

---
+++ models/testCurrencyList.js
@@ -0,0 +1,97 @@
+module.exports = currencyList = [
+    {
+        ch_tw: '美金',
+        en: 'USD',
+        index: 1
+    },
+    {
+        ch_tw: '港幣',
+        en: 'HKD',
+        index: 2
+    },
+    {
+        ch_tw: '英鎊',
+        en: 'GBP',
+        index: 3
+    },
+    {
+        ch_tw: '澳幣',
+        en: 'AUD',
+        index: 4
+    },
+    {
+        ch_tw: '加拿大幣',
+        en: 'CAD',
+        index: 5
+    },
+    {
+        ch_tw: '新加坡幣',
+        en: 'SGD',
+        index: 6
+    },
+    {
+        ch_tw: '瑞士法郎',
+        en: 'CHF',
+        index: 7
+    },
+    {
+        ch_tw: '日圓',
+        en: 'JPY',
+        index: 8
+    },
+    {
+        ch_tw: '南非幣',
+        en: 'ZAR',
+        index: 9
+    },
+    {
+        ch_tw: '瑞典幣',
+        en: 'SEK',
+        index: 10
+    },
+    {
+        ch_tw: '紐元',
+        en: 'NZD',
+        index: 11
+    },
+    {
+        ch_tw: '泰幣',
+        en: 'THB',
+        index: 12
+    },
+    {
+        ch_tw: '菲國比索',
+        en: 'PHP',
+        index: 13
+    },
+    {
+        ch_tw: '印尼幣',
+        en: 'IDR',
+        index: 14
+    },
+    {
+        ch_tw: '歐元',
+        en: 'EUR',
+        index: 15
+    },
+    {
+        ch_tw: '韓元',
+        en: 'KRW',
+        index: 16
+    },
+    {
+        ch_tw: '越南盾',
+        en: 'VND',
+        index: 17
+    },
+    {
+        ch_tw: '馬來幣',
+        en: 'MYR',
+        index: 18
+    },
+    {
+        ch_tw: '人民幣',
+        en: 'CNY',
+        index: 19
+    }
+];
