8c73c38c6098665bdc2365fb1e93583c3eedcd55
Jimmy <born1031.jimmy@gmail.com>
2020-03-24 23:20:19 +0800

    v1.0.1 test

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
-var linebot = require('linebot');
 var express = require('express');
-var messenger = require('./controllers/messageController');
-
-var bot = linebot({
-    channelId: '1653936288',
-    channelSecret: '9914a93854b110443120b46240eae039',
-    channelAccessToken: '72jIJyLaqwIKgS5XDmxFx++2s2lcMQXp9ARTPBC7BeHRlObrN7swCwwfJPDcso6nr94n5F3EbXkDCPhWD4325w4fBkRyvYL5I0Uq7bJiOz+Yeiof37Hi4WYZpzoIThvUW+zCppKpLF5pGmIoQ6phjwdB04t89/1O/w1cDnyilFU='
-});
-
-_bot();
-
+const linebot = require('./line_bot');
+const linebotParser = linebot.parser();
 const app = express();
-const linebotParser = bot.parser();
+
+linebot._bot();
 app.post('/', linebotParser);
 
 // Convert port for heroku
@@ -19,14 +11,3 @@ var server = app.listen(process.env.PORT || 8080, function () {
     var port = server.address().port;
     console.log('App now running on port: ' + port);
 });
-
-function _bot() {
-    // User has sent message.
-    bot.on('message', function (event) {
-        // Print event.
-        console.log(event);
-
-        // Reply messages in messageController.
-        messenger(event);
-    });
-};
