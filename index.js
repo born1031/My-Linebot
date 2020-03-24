cb32c8d41ce1c5ecddc26558f57c082373489f27
Jimmy <born1031.jimmy@gmail.com>
2020-03-24 23:56:24 +0800

    v1.0.2 test

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 var express = require('express');
-const linebot = require('./line_bot');
+var linebot = require('linebot');
+var messenger = require('./controllers/messageController');
+
+var bot = linebot({
+    channelId: '1653936288',
+    channelSecret: '9914a93854b110443120b46240eae039',
+    channelAccessToken: '72jIJyLaqwIKgS5XDmxFx++2s2lcMQXp9ARTPBC7BeHRlObrN7swCwwfJPDcso6nr94n5F3EbXkDCPhWD4325w4fBkRyvYL5I0Uq7bJiOz+Yeiof37Hi4WYZpzoIThvUW+zCppKpLF5pGmIoQ6phjwdB04t89/1O/w1cDnyilFU='
+});
+
 const linebotParser = linebot.parser();
 const app = express();
 
-linebot._bot();
+_bot();
 app.post('/', linebotParser);
 
 // Convert port for heroku
-var server = app.listen(process.env.PORT || 8080, function () {
+var server = app.listen(process.env.PORT || 8080, function(){
     var port = server.address().port;
     console.log('App now running on port: ' + port);
 });
+
+function _bot() {
+    // User has sent message.
+    bot.on('message', function (event) {
+        // Print event.
+        console.log(event);
+
+        // Reply messages in messageController.
+        messenger(event);
+    });
+};
--- modules/foodSelector.js
+++ modules/foodSelector.js
@@ -1,9 +1,9 @@
 var foodList = require('../models/food_list');
 
-function _getRandomFood(type) {
+function _getRandomFood(type){
     var foodType = type;
     var tempList = foodList.filter(foodList => foodList.type.indexOf(foodType) >= 0);
-
+    
     // Get a random number.
     var maxNum = tempList.length - 1;
     var randomNum = Math.floor(Math.random() * maxNum);
